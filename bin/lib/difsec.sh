1c66eab7c1dd2229a6fe4c0de316bcdb367faae0
Michael Surbey <msurbey@redhat.com>
2018-01-08 15:40:41 -0500

    Seperated into independent files, or renamed

--- bin/lib/difsec.sh
+++
@@ -1,8 +0,0 @@
-
-function difsec() {
- local fromtime=$1; shift || return 1
- local currtime
- currtime=$(date +%s)
-
- echo "$(expr $currtime - $fromtime)"
-}
--- bin/lib/isacmd.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-function isacmd() {
- local mycomm=$1; shift || return 1
-
- hash $mycomm 2>/dev/null || return 1
-}
