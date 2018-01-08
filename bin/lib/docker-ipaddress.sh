69fe187dac92087118dee8f51c0d52247a7a4ed5
Michael Surbey <msurbey@redhat.com>
2018-01-08 15:44:13 -0500

    Checkpoint

--- bin/lib/docker-ipaddress.sh
+++ bin/lib/docker-ipaddress.sh
@@ -1,6 +1,6 @@
 docker-ipaddress(){
  local fmt='--format {{ .NetworkSettings.IPAddress }}'
- local cmd="docker ps ${1:-}"
+ local cmd="docker ps ${1:-} $fmt"
 
  if ! $cmd; then
   printf "fail: ${FUNCNAME[0]}: %s\n" "$cmd" >&2; return 1
--- bin/lib/docker-name.sh
+++
@@ -1,7 +0,0 @@
-docker-last-name(){
- local cmd="docker ps -aqf name=${1:-}"
-
- if ! $cmd; then
-  printf "fail: ${FUNCNAME[0]}: %s\n" "$cmd" >&2; return 1
- fi
-}
