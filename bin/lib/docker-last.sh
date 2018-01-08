9c207a26a9746317efdaeb62a2c89a1ba9bdceef
Michael Surbey <msurbey@redhat.com>
2018-01-08 15:42:08 -0500

    Fixed command

--- bin/lib/docker-last.sh
+++ bin/lib/docker-last.sh
@@ -1,6 +1,5 @@
 docker-last(){
- local fmt='--format {{.Names}}'
- local cmd="docker ps -n 1 $fmt"
+ local cmd="docker ps -n 1"
 
  if ! $cmd; then
   printf "fail: ${FUNCNAME[0]}: %s\n" "$cmd" >&2; return 1
