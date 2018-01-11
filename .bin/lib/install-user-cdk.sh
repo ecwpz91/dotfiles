a6f44f58b4c7a5d53dc2ca1f207fdec8dcc38a44
Michael Surbey <msurbey@redhat.com>
2018-01-10 23:06:58 -0500

    Renamed

---
+++ .bin/lib/install-user-cdk.sh
@@ -0,0 +1,45 @@
+install-minishift-binary(){
+ local bincdk=${1:-'HOME/Downloads/cdk-3.2.0-1-minishift*'}
+ local cdkuri="https://developers.redhat.com/download-manager/file/cdk-3.2.0-1-minishift-linux-amd64"
+
+ if ! -f $bincdk; then
+  printf "%s\n" "Vist $cdkuri for CDK download" >&2; return 1;
+ fi
+
+ # Add yourself to the libvirt group so that you do not need to sudo
+ printf "%s " "root@${HOSTNAME}"; su - root -c 'yum install libvirt qemu-kvm -y
+	                                         newgrp libvirt
+	                                         usermod -a -G libvirt ${USER}
+	                                         systemctl start libvirtd
+	                                         systemctl enable libvirtd
+	                                         curl -L https://github.com/dhiltgen/docker-machine-kvm/releases/download/v0.7.0/docker-machine-driver-kvm -o /usr/local/bin/docker-machine-driver-kvm
+	                                         chmod +x /usr/local/bin/docker-machine-driver-kvm
+ '
+
+ # Check if Docker machine exists, if not install locally
+ # sudo curl -L https://github.com/docker/machine/releases/download/v0.10.0/docker-machine-`uname -s`-`uname -m` -o /usr/local/bin/docker-machine \
+ # && sudo chmod +x /usr/local/bin/docker-machine-driver-kvm
+
+ # Check if minishift exists, if not install locally
+ mv $bincdk $HOME/.local/bin/minishift \
+ && chmod +x $HOME/.local/bin/minishift
+
+ [ ! -d $HOME/.minishift ] \
+ && minishift setup-cdk \
+ && minishift config set memory 4096 &>/dev/null \
+ && minishift config set cpus 2  &>/dev/null
+
+ get_export() {
+ cat <<EOF
+# ----------------------------------------------------------------------
+# Minishift
+# ----------------------------------------------------------------------
+# export MINISHIFT_VM_DRIVER=
+# export MINISHIFT_USERNAME=
+# export MINISHIFT_PASSWORD=
+
+EOF
+ }
+
+ echo "$(get_export)" >> $HOME/.bashrc
+}
---
+++ .bin/lib/make-bash-script.sh
@@ -0,0 +1,74 @@
+make-bash-script() {
+ local fcn="${FUNCNAME[0]}"
+ local des="Create a new bash script"
+ local maj='1'
+ local min='0'
+ local pat='0'
+ local ver="${maj}.${min}"
+ local rel="${ver}.${pat}"
+
+ # Mark read only vars
+ declare -r fcn des maj min pat ver rel
+
+ usage() {
+	  cat <<EOF
+Usage: ${FCN:-} [FILENAME]
+
+${des:-}
+
+Options:
+  -D, --debug                     Enable debug mode
+  -h, --help                      Print usage
+  -v, --version                   Print version information and quit
+EOF
+
+  [ ${debug:-} ] && syslog "Debug mode true"
+ }
+
+ release(){
+  printf "%s, release %s\n" "${fcn:-}" "${rel:-}"
+ }
+
+ syslog() {
+  local msg=${1:-'No information provided'}; shift
+  local log=${1:-'DEBUG'}
+
+  printf "%s [%s] %s\n" "$(date +%T)" "${log}" "${msg}"
+ }
+
+ main() {
+  local OPTARG OPTIND opt debug targs mycmd
+
+  while getopts ":-:Dv" opt; do
+   case ${opt} in
+    -  ) case "${OPTARG:-}" in
+      debug   ) debug='true' ;;
+      version ) release; return 1 ;;
+      *       ) usage; return 1
+      ;;
+    esac ;;
+    D  ) debug='true' ;;
+    v  ) release; return 1 ;;
+    \? ) usage; return 1
+   esac
+  done
+
+  shift $(($OPTIND - 1));
+
+  # Check null argument
+  if [[ -z "${@}" ]]; then
+   [ ${debug:-} ] && syslog "Null input args"
+   usage; return 1
+  fi
+
+  [ ${debug:-} ] && syslog "Run"
+
+  for i in "${@}"; do
+   [ ${debug:-} ] && syslog "Exec input args"
+  done
+
+  [ ${debug:-} ] && syslog "End"
+ }
+
+ main "${@}"
+}
