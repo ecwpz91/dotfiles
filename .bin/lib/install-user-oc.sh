1b55a39812f8f40d817802fc3c5ffea54d3c2128
Michael Surbey <msurbey@redhat.com>
2018-01-11 14:10:27 -0500

    Updated

--- .bin/lib/install-user-oc.sh
+++ .bin/lib/install-user-oc.sh
@@ -1,5 +1,5 @@
-install-user-oc(){
- curl -L "https://mirror.openshift.com/pub/openshift-v3/clients/${1:-3.6.173.0.96}/linux/oc.tar.gz" | tar -xvzf - -C $HOME/.local/bin --strip 1 \
+install-user-oc() {
+ curl -L -O 'https://mirror.openshift.com/pub/openshift-v3/clients/3.7.22/linux/oc.tar.gz' | tar -xvzf - -C $HOME/.local/bin \
  && chmod +x $HOME/.local/bin/oc \
  && rm -rf $HOME/.local/bin/oc.sh &>/dev/null \
  && oc completion bash > $HOME/.local/bin/oc.sh
