08c0ed54c96395fb7855754e5972734d71c149fa
Michael Surbey <msurbey@redhat.com>
2018-01-11 13:45:08 -0500

    Fixed binary curl output by removing -O (do not output a file)

--- .bin/lib/install-user-oc.sh
+++ .bin/lib/install-user-oc.sh
@@ -1,11 +1,5 @@
 install-user-oc(){
-	[[ "$(uname -m)" == "x86_64" ]] \
- && ost="$(uname)";ost="${ost,,}" \
-	&& app='oc' \
-	&& ver=${1:-'3.6.173.0.96'} \
-	&& ext='tar.gz'
-
- curl -L -O 'https://mirror.openshift.com/pub/openshift-v3/clients/3.6.173.0.27/linux/oc.tar.gz' | tar -xvzf - -C $HOME/.local/bin --strip 1 \
+ curl -L "https://mirror.openshift.com/pub/openshift-v3/clients/${1:-3.6.173.0.96}/linux/oc.tar.gz" | tar -xvzf - -C $HOME/.local/bin --strip 1 \
  && chmod +x $HOME/.local/bin/oc \
  && rm -rf $HOME/.local/bin/oc.sh &>/dev/null \
  && oc completion bash > $HOME/.local/bin/oc.sh
