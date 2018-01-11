cdd6cf283ef63e768364e671d1fce0714fa3d0a7
Michael Surbey <msurbey@redhat.com>
2018-01-10 23:19:35 -0500

    Renamed

--- .bin/lib/docker-infinity.sh
+++
@@ -1,5 +0,0 @@
-docker-infinity() {
- local name=${1:-}; shift || return 1
-
- docker run -d ${name:-} /bin/bash -c 'while true; do sleep 1000; done'
-}
--- .bin/lib/get-user-pip.sh
+++
@@ -1,9 +0,0 @@
-function get-user-pip() {
- if ! hash pip 2>/dev/null; then
-  temp=$(mktemp -d) \
-  && pushd ${temp} \
-  && wget https://bootstrap.pypa.io/get-pip.py \
-  && python get-pip.py --user \
-  && popd && rm -rf ${temp}
- fi
-}
