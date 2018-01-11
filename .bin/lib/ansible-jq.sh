5b0be7c1bbf13aa57f3feb9e9db193e28c8ba913
Michael Surbey <msurbey@redhat.com>
2018-01-10 23:13:11 -0500

    Broke local jq install into independent script

--- .bin/lib/ansible-jq.sh
+++ .bin/lib/ansible-jq.sh
@@ -38,22 +38,7 @@ EOF
   if [ $? != 0 ] ; then
    printf "%s\n" "$func_id: Command failed '$result'" && return 1
   fi
-
-  # Checks if command in hash table exists before executing it
-  shopt -s checkhash
-
-  if ! hash jq 2>/dev/null ; then
-   temp=$(mktemp -d) \
-   && pushd ${temp} &>/dev/null \
-   && wget https://github.com/stedolan/jq/releases/download/jq-1.5/jq-linux64 -O jq \
-   && cp -rf jq $HOME/.local/bin/jq \
-   && chmod +x $HOME/.local/bin/jq \
-   && popd &>/dev/null \
-   && rm -rf ${temp}
-  fi
-
-  shopt -u checkhash
-
+  
   printf "%s" "${result#*=>}" | jq
  done
 }
