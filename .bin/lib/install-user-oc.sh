53ba215a4f2a7d6e9b83c53d0bde717ca2c3a85c
Michael Surbey <msurbey@redhat.com>
2018-01-11 14:24:36 -0500

    Updated with latest supported version of OpenShift enterprise

--- .bin/lib/install-user-oc.sh
+++ .bin/lib/install-user-oc.sh
@@ -1,5 +1,8 @@
 install-user-oc() {
- curl -L -O 'https://mirror.openshift.com/pub/openshift-v3/clients/3.7.22/linux/oc.tar.gz' | tar -xvzf - -C $HOME/.local/bin \
+ pushd /tmp &>/dev/null \
+ && curl -L -O "https://mirror.openshift.com/pub/openshift-v3/clients/3.7.14/linux/oc.tar.gz" \
+ && tar -xvzf oc.tar.gz -C $HOME/.local/bin &>/dev/null \
+ && popd &>/dev/null \
  && chmod +x $HOME/.local/bin/oc \
  && rm -rf $HOME/.local/bin/oc.sh &>/dev/null \
  && oc completion bash > $HOME/.local/bin/oc.sh
