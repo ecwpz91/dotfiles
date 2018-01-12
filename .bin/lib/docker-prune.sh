d038c772ae7fbac503aaee9f5546ed0e8610357f
Michael Surbey <msurbey@redhat.com>
2018-01-11 23:39:21 -0500

    Updated

--- .bin/lib/docker-prune.sh
+++ .bin/lib/docker-prune.sh
@@ -1,5 +1,5 @@
 docker-prune() {
- docker rm "$(docker ps -aqf status=exited)" 2>/dev/null
- docker rmi -f "$(docker images -a -f dangling=true)" 2>/dev/null
+ docker rm -f "$(docker ps -aq)" 2>/dev/null
+ docker rmi -f "$(docker images -aqf dangling=true)" 2>/dev/null
  docker volume rm "$(docker volume ls -f dangling=true)" 2>/dev/null
 }
