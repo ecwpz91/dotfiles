d710863d866af8075d5f1e03fd321a8682c37bab
Michael Surbey <msurbey@redhat.com>
2018-01-16 09:28:04 -0500

    Updated to source bash command complete

--- .bin/lib/pip-install-user-awscli.sh
+++ .bin/lib/pip-install-user-awscli.sh
@@ -5,6 +5,11 @@ pip-install-user-awscli() {
 
  if hash pip 2>/dev/null; then
   pip install --upgrade --user pip awscli boto boto3
+
+  if ! grep -R aws_completer $HOME/.bashrc &>/dev/null; then
+   printf "%s\n" "complete -C $HOME/.local/bin/aws_completer aws"  >> $HOME/.bashrc
+   source $HOME/.bashrc
+  fi
  fi
 
  [[ ${flagset:-} ]] && shopt -u checkhash; flagset=false
