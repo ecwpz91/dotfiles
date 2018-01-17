5893c065760ee77b13963c4206beff25bc99b2bf
Michael Surbey <msurbey@redhat.com>
2018-01-17 10:18:35 -0500

    Formatted var definition

--- .bin/lib/diff-secs.sh
+++ .bin/lib/diff-secs.sh
@@ -2,8 +2,7 @@
 
 diff-secs() {
  local init=$1; shift || return 1
- local secs=$((`TZ=GMT0 date \
- +"((%Y-1600)*365+(%Y-1600)/4-(%Y-1600)/100+(%Y-1600)/400+%j-135140) *86400+%H*3600+%M*60+%S"`))
+ local secs=$((`TZ=GMT0 date +"((%Y-1600)*365+(%Y-1600)/4-(%Y-1600)/100+(%Y-1600)/400+%j-135140) *86400+%H*3600+%M*60+%S"`))
 
  echo "$(expr $secs - $init)"
 }
