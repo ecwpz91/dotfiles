8d0eeee51fb48dd80d54323e1aee3adfc040e914
Michael Surbey <msurbey@redhat.com>
2018-01-29 12:42:33 -0500

    Initial revision

---
+++ .local/bin/init-user-fhc.sh
@@ -0,0 +1,30 @@
+init-user-fhc() {
+
+ bashexp(){
+  cat <<EOF
+  # ----------------------------------------------------------------------
+  # Node.js
+  # ----------------------------------------------------------------------
+  export N_PREFIX="$HOME/.n"; [[ :$PATH: == *":$N_PREFIX/bin:"* ]] || PATH+=":$N_PREFIX/bin" # Added by n-install (see http://git.io/n-install-repo).
+EOF
+ }
+
+ main() {
+  local flagset
+
+  [[ ! $(shopt checkhash &>/dev/null) ]] && shopt -s checkhash; flagset=true
+
+  if ! hash n 2>/dev/null ; then
+   echo "$(bashexp)" >> $HOME/.bashrc
+   source $HOME/.bashrc
+   curl -L https://git.io/n-install | bash
+  fi
+
+  npm install -g fh-fhc
+  fhc completion bash > $HOME/.local/bin/fhc.sh
+
+  [[ ${flagset:-} ]] && shopt -u checkhash; flagset=false
+ }
+
+ main "${@}"
+}
