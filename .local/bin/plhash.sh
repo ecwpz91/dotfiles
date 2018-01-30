7258ed11883b375670a1874d666b2334d3372866
Michael Surbey <msurbey@redhat.com>
2018-01-30 14:26:13 -0500

    Renamed & added check for python library

--- .local/bin/plhash.sh
+++
@@ -1,8 +0,0 @@
-function plhash() {
- local passwd=$1; shift || return 1
- local script
-
- script="from passlib.hash import sha512_crypt;print sha512_crypt.using(rounds=5000).hash(\"$passwd\")"
-
- python -c "$script"
-}
