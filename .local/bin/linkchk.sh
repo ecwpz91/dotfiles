4b5246ce181bd5f1dc7c2282d63552d02f1521cc
Michael Surbey <msurbey@redhat.com>
2018-01-30 21:52:47 -0500

    Quoted env parameter

--- .local/bin/linkchk.sh
+++ .local/bin/linkchk.sh
@@ -1,6 +1,6 @@
-linkchk () {
+linkchk() {
  for element in $1/*; do
   [ -h "$element" -a ! -e "$element" ] && echo \"$element\"
-  [ -d "$element" ] && linkchk $element
+  [ -d "$element" ] && linkchk "$element"
  done
 }
