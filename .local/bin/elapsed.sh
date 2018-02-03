904973da6c154322638761410326664c96d9f274
ecwpz91 <ecwpz91@users.noreply.github.com>
2018-02-02 23:34:19 -0500

    Final consolidation effort

--- .local/bin/elapsed.sh
+++
@@ -1,5 +0,0 @@
-elapsed() {
- local sec=$1; shift || return 1
-
- printf "%s\n" "$(expr $((`TZ=GMT0 date +"((%Y-1600)*365+(%Y-1600)/4-(%Y-1600)/100+(%Y-1600)/400+%j-135140) *86400+%H*3600+%M*60+%S"`)) - $sec)"
-}
--- .local/bin/rpm-gpg-pubkey.sh
+++ .local/bin/rpm-gpg-pubkey.sh
@@ -1,5 +1,4 @@
 rpm-gpg-pubkey() {
  # https://linuxconfig.org/how-to-list-import-and-remove-archive-signing-keys-on-centos-7
- 
  rpm -q gpg-pubkey --qf '%{NAME}-%{VERSION}-%{RELEASE}\t%{SUMMARY}\n'
 }
