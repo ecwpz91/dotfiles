ea44d4d3c2d7e07945d7a4ac03c5715cc0eb58c8
ecwpz91 <ecwpz91@users.noreply.github.com>
2018-02-08 18:28:42 -0500

    Removed conditional check (overwrite with latest)

--- .local/bin/install-pip.sh
+++ .local/bin/install-pip.sh
@@ -1,9 +1,7 @@
 install-pip() {
- if ! type pip &>/dev/null; then
-  temp=$(mktemp -d) \
-  && pushd "$temp" \
-  && wget https://bootstrap.pypa.io/get-pip.py \
-  && python get-pip.py --user \
-  && popd && rm -rf "$temp"
- fi
+ temp=$(mktemp -d) \
+ && pushd "$temp" \
+ && wget https://bootstrap.pypa.io/get-pip.py \
+ && python get-pip.py --user \
+ && popd && rm -rf "$temp"
 }
