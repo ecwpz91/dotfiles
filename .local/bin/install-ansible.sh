975ac50dc3df4f880475bec40b43ce54a3bdf972
ecwpz91 <ecwpz91@users.noreply.github.com>
2018-02-08 18:39:15 -0500

    Favor curl over wget due to better cross platform compatability

--- .local/bin/install-ansible.sh
+++ .local/bin/install-ansible.sh
@@ -1,10 +1,10 @@
 install-ansible() {
  if ! type pip &>/dev/null; then
   temp=$(mktemp -d) \
-  && pushd "$temp" \
-  && wget https://bootstrap.pypa.io/get-pip.py \
+  && pushd $temp &>/dev/null \
+  && curl -LO 'https://bootstrap.pypa.io/get-pip.py' \
   && python get-pip.py --user \
-  && popd && rm -rf "$temp"
+  && popd &>/dev/null && rm -rf $temp
  fi
 
  pip install --upgrade --user pip ansible cryptography passlib
