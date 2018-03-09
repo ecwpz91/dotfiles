67a60b0841a84fcc11177de74d867f1a44711a27
ecwpz91 <ecwpz91@users.noreply.github.com>
2018-03-09 02:29:37 -0500

    Checkpoint

--- .local/bin/system-ansible-vault.sh
+++ .local/bin/system-ansible-vault.sh
@@ -8,7 +8,8 @@ function system-ansible-vault() {
   local minver
   local patver
   local tmpdir
-  local osrelv
+  local ospkgs
+  local osrels
 
   # Get Python version information
   script='import sys; print(sys.version_info[0])'
@@ -21,28 +22,22 @@ function system-ansible-vault() {
   patver=$(python -c "$script")
 
   if [ "${majver%.*}" -le "2" ] && [ "${minver#*.}" -le "7" ] && [ "${patver%.*}" -lt 9 ]; then
-   printf "Python %s.%s.%s <2.7.9\n" "$majver" "$minver" "$patver" \
-   && osrelv=$(rpm -q --qf '%{VERSION}' "$(rpm -q --whatprovides redhat-release)") \
-   && { osrelv="${osrelv//[!6-7]}" >&2; } \
-   && tmpdir=$(mktemp -d) \
-   && pushd $tmpdir &>/dev/null \
-   && curl -LO "https://dl.fedoraproject.org/pub/epel/epel-release-latest-${osrelv}.noarch.rpm" \
-   && rpm -Uvh "epel-release-latest-${osrelv}.noarch.rpm" --force \
-   && { [[ $(yum list installed python-pip 2>/dev/null) ]] && yum -y remove python-pip >&2; } \
-   && { [[ $(yum list installed python-setuptools 2>/dev/null) ]] && yum -y upgrade python-setuptools >&2; } \
-   && { [[ "${osrelv}" == '7' ]] && yum -y install python-setuptools python-wheel >&2; } && \
-   # Uncomment next two lines to install pip with get-pip.py:
-   # curl -LO 'https://bootstrap.pypa.io/get-pip.py' && \
-   # { [[ "${osrelv}" == '7' ]] && python get-pip.py --no-setuptools --no-wheel || python get-pip.py --no-setuptools >&2; } && \
-   yum -y install python-pip python-cryptography python-passlib ansible && \
-   # [NOTE]: Be cautious if you're using a Python install that's managed by your operating
-   # system or another package manager. get-pip.py does not coordinate with those tools,
-   # and may leave your system in an inconsistent state.
-   pip install --upgrade pip cryptography passlib ansible \
-   && popd &>/dev/null \
-   && rm -rf $tmpdir \
-   && source /etc/environment \
-   && source /etc/profile
+   printf "Python %s.%s.%s <2.7.9" "$majver" "$minver" "$patver" && \
+    osrelv=$(rpm -q --qf '%{VERSION}' "$(rpm -q --whatprovides redhat-release)") && \
+    { osrelv="${osrelv//[!6-7]}" >&2; } && \
+    tmpdir=$(mktemp -d) && \
+    pushd $tmpdir &>/dev/null && \
+    curl -LO "https://dl.fedoraproject.org/pub/epel/epel-release-latest-${osrelv}.noarch.rpm" && \
+    rpm -Uvh "epel-release-latest-${osrelv}.noarch.rpm" --force && \
+    curl -LO 'https://bootstrap.pypa.io/get-pip.py' && \
+    { [[ $(yum list installed python-setuptools 2>/dev/null) ]] && yum -y upgrade python-setuptools >&2; } && \
+    { [[ "${osrelv}" == '7' ]] && yum -y install python-wheel >&2; } && \
+    ospkgs='python2 python-pip python-cryptography python-yaml python-simplejson ansible' && \
+    yum -y install $ospkgs && \
+    { [[ "${osrelv}" == '7' ]] && python get-pip.py --no-setuptools --no-wheel || python get-pip.py --no-setuptools >&2; } && \
+    popd &>/dev/null && rm -rf $tmpdir && \
+    pypkgs='pip cryptography pyyaml simplejson passlib passlib[bcrypt] passlib[argon2] passlib[totp] fastpbkdf2 scrypt ansible' && \
+    pip install --upgrade $pypkgs
   fi
  }
 
@@ -50,54 +45,26 @@ function system-ansible-vault() {
   local passwd=${1:-}; shift || return 1
   local script
 
-  script='import passlib'
-
-  if ! python -c "$script" &>/dev/null; then
-   printf "%s\n" "Error: not a valid python module" \
-   && return 1
-   # See https://passlib.readthedocs.io/en/stable/install.html#installation-instructions
+  if ! python -c 'import passlib' 2>/dev/null; then
+   { printf "\n\%s" "Error: python module missing" >&2; return 1; };
   fi
 
-  script="from passlib.hash import sha512_crypt;print sha512_crypt.using(rounds=5000).hash(\"$passwd\")"
+  script="from passlib.handlers.sha2_crypt import sha512_crypt;print sha512_crypt.using(rounds=5000).hash(\"$passwd\")"
 
   python -c "$script"
- }
-
- get_system_ansible_vault_password() {
-  local mypass
-  local chkstr
-
-  printf "\%s\n" "Changing password for Ansible Vault"
-  printf "\%s" "Enter new vault password:"
-  read mypass
-
-  printf "\%s" "Retype new vault password:"
-  read chkstr
-
-  if [[ "${mypass}" !=  "${chkstr}" ]]; then
-   printf "%s\n" "Error: passwords did not match" \
-   && return 1
-  fi
-
-  printf "%s" "$(passlib_hash_string $mypass)"
  }
 
  set_system_ansible_config_vault_password_file() {
   local myfile=${1:-}
   local myconf=${2:-}; shift 2 || return 1
-  local bakext
   local rgxstr
 
   if ! touch "$myconf"; then
-   printf "%s\n" "Error: couldn't create file" \
-   && return 1
+   { printf "\n\%s" "Error: couldn't create file" >&2; return 1; };
   fi
 
-  bakext="$((`date +((%Y-1600)*365+(%Y-1600)/4-(%Y-1600)/100+(%Y-1600)/400+%j-135140)*86400+%H*3600+%M*60+%S`))"
-
-  if ! cp "$myconf" "$myconf.$backup"; then
-   printf "%s\n" "Error: couldn't backup file" \
-   && return 1
+  if ! cp "$myconf" "$myconf.${RANDOM}"; then
+   { printf "\n\%s" "Error: couldn't backup file" >&2; return 1; };
   fi
 
   rgxstr='vault_password_file'
@@ -108,15 +75,25 @@ function system-ansible-vault() {
  }
 
  main() {
-  local vaultf
-  local syscfg
-
-  vaultf='/etc/ansible/vault'
-  syscfg='/etc/ansible/ansible.cfg'
+  local mypass
+  local chkstr
 
   get_system_ansible
-  printf "%s" "$(get_system_ansible_vault_password)" > "$vaultf"
-  set_system_ansible_config_vault_password_file "$vaultf" "$syscfg"
+
+  printf "\n\%s" "Changing password for Ansible Vault"
+  printf "\n\%s" "Enter new vault password: "
+  read -s mypass
+
+  printf "\n\%s" "Retype new vault password: "
+  read -s chkstr
+
+  if [[ "$mypass" !=  "$chkstr" ]]; then
+   { printf "\n\%s" "Error: passwords did not match" >&2; return 1; };
+  fi
+
+  printf "%s" "$(passlib_hash_string $mypass)" > /etc/ansible/vault
+
+  # set_system_ansible_config_vault_password_file "/etc/ansible/vault" "/etc/ansible/ansible.cfg"
  }
 
  main "$@"
