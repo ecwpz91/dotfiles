c7b9518c1a4b8092d246a9b954fa5a7698da3e7d
ecwpz91 <ecwpz91@users.noreply.github.com>
2018-04-29 23:58:37 -0400

    Used explicit command option (instead of shorthand)

--- .local/bin/rpm-gpg-pubkey.sh
+++ .local/bin/rpm-gpg-pubkey.sh
@@ -1,4 +1,3 @@
 rpm-gpg-pubkey() {
- # https://linuxconfig.org/how-to-list-import-and-remove-archive-signing-keys-on-centos-7
- rpm -q gpg-pubkey --qf '%{NAME}-%{VERSION}-%{RELEASE}\t%{SUMMARY}\n'
+ rpm --query gpg-pubkey --queryformat '%{NAME}-%{VERSION}-%{RELEASE}\t%{SUMMARY}\n'
 }
