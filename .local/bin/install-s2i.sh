9cb5e44121a40bba5ea168e8bb3c6985c4bb4f79
ecwpz91 <ecwpz91@users.noreply.github.com>
2018-05-03 18:22:01 -0400

    Initial revision

---
+++ .local/bin/install-s2i.sh
@@ -0,0 +1,4 @@
+install-s2i() {
+ curl -L 'https://github.com/openshift/source-to-image/releases/download/v1.1.9a/source-to-image-v1.1.9a-40ad911d-linux-amd64.tar.gz' \
+ | tar -xvzf - -C $HOME/.local --strip 1 &>/dev/null
+}
