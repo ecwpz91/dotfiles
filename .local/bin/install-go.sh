704972d56f680d7e22ce783c2679bb568a619f2f
ecwpz91 <ecwpz91@users.noreply.github.com>
2018-06-15 14:17:58 -0400

    Update install-go.sh
    Updated minor release

--- .local/bin/install-go.sh
+++ .local/bin/install-go.sh
@@ -1,4 +1,4 @@
 install-go() {
- curl -L 'https://dl.google.com/go/go1.10.1.linux-amd64.tar.gz' \
+ curl -L 'https://dl.google.com/go/go1.10.3.linux-amd64.tar.gz' \
  | tar -xvzf - -C $HOME/.local --strip 1 &>/dev/null
 }
