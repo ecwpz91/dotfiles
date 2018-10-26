b4def2a1c7e0647865f0d29ee77a021e942bf314
ecwpz91 <ecwpz91@users.noreply.github.com>
2018-10-26 16:49:14 -0400

    Reset for compatability with oc-cluster wrapper script

--- .local/bin/install-oc.sh
+++ .local/bin/install-oc.sh
@@ -1,4 +1,4 @@
 install-oc() {
- curl -L 'https://mirror.openshift.com/pub/openshift-v3/clients/3.10.14/linux/oc.tar.gz' \
+ curl -L 'https://mirror.openshift.com/pub/openshift-v3/clients/3.9.14/linux/oc.tar.gz' \
  | tar -xvzf - -C "$HOME/.local/bin" &>/dev/null
 }
