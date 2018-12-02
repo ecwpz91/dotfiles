b7d7e80d7d6774f16c737c9e0dc51c87549e3e5a
ecwpz91 <ecwpz91@users.noreply.github.com>
2018-12-01 22:06:49 -0500

    Added comment for installing bash completion

--- .local/bin/install-s2i.sh
+++ .local/bin/install-s2i.sh
@@ -1,4 +1,7 @@
 install-s2i() {
  curl -L 'https://github.com/openshift/source-to-image/releases/download/v1.1.9a/source-to-image-v1.1.9a-40ad911d-linux-amd64.tar.gz' \
  | tar -xvzf - -C $HOME/.local --strip 1 &>/dev/null
+
+ # Install bash completion
+ # s2i completion bash > ~/.profile.d/s2i.sh
 }
