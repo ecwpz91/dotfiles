2d4f8d2e073eec6e22f394202cb780044de99ac4
ecwpz91 <ecwpz91@users.noreply.github.com>
2018-12-10 14:59:40 -0500

    Moved to .bashrc

--- .local/bin/cmdcom.sh
+++
@@ -1,7 +0,0 @@
-cmdcom() {
- local cmd=$1; shift || return 1
-
- [ -d ~/.profile.d ] \
- && rm -rf "$HOME/.profile.d/$cmd-completion" &>/dev/null \
- && $cmd completion bash > "$HOME/.profile.d/$cmd-completion"
-}
