16896a426e443d2e7d042d285f377af5f17e8b5e
ecwpz91 <ecwpz91@users.noreply.github.com>
2020-09-28 13:40:50 -0400

    Updated to latest release

--- .local/bin/install-maven.sh
+++ .local/bin/install-maven.sh
@@ -1,10 +1,10 @@
 install-maven() {
  [[ ! -d "$HOME/.local/share/maven" ]] && mkdir -p "$HOME/.local/share/maven"
 
- curl -L 'http://www.gtlib.gatech.edu/pub/apache/maven/maven-3/3.6.0/binaries/apache-maven-3.6.0-bin.tar.gz' \
+ curl -L 'https://mirrors.gigenet.com/apache/maven/maven-3/3.6.3/binaries/apache-maven-3.6.3-bin.tar.gz' \
  | tar -xzf - -C "$HOME/.local/share/maven" --strip 1
 
- ln -s "$HOME/.local/share/maven/bin/mvn" "$HOME/.local.local/bin/mvn"
+ ln -s "$HOME/.local/share/maven/bin/mvn" "$HOME/.local/bin/mvn"
 
  # Install bash completion
  # curl -L 'https://raw.githubusercontent.com/juven/maven-bash-completion/master/bash_completion.bash' \
