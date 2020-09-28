98122d9660007dbfb0e6cc208629705d4431dd72
ecwpz91 <ecwpz91@users.noreply.github.com>
2020-09-28 13:46:22 -0400

    Updated release and location of install

--- .local/bin/install-openshift-install.sh
+++ .local/bin/install-openshift-install.sh
@@ -1,5 +1,11 @@
 install-openshift-install() {
- curl -L 'https://github.com/openshift/installer/releases/download/v0.11.0/openshift-install-linux-amd64' \
+ curl -L 'https://github.com/openshift/installer/releases/download/v0.12.0/openshift-install-linux-amd64' \
       -o "$HOME/.local/bin/openshift-install" \
  && chmod +x "$HOME/.local/bin/openshift-install"
+
+ curl -L 'https://mirror.openshift.com/pub/openshift-v4/clients/ocp/latest/openshift-install-linux-4.1.8.tar.gz' \
+ | tar -xvzf - -C "$HOME/.local/bin" &>/dev/null
+
+ # Install bash completion
+ # oc completion bash > ~/.profile.d/oc.sh
 }
