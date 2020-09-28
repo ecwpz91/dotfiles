9d61cf624ea805f44ed4cfcfa56fd0d16b37ddbe
ecwpz91 <ecwpz91@users.noreply.github.com>
2020-09-28 13:45:30 -0400

    Updated location of odo binary release curl command

--- .local/bin/install-odo.sh
+++ .local/bin/install-odo.sh
@@ -1,4 +1,4 @@
 install-odo() {
- curl -L 'https://github.com/redhat-developer/odo/releases/download/v0.0.14/odo-linux-amd64' -o "$HOME/.local/bin/odo" \
+ curl -L 'https://mirror.openshift.com/pub/openshift-v4/clients/odo/latest/odo-linux-amd64' -o "$HOME/.local/bin/odo" \
  && chmod +x "$HOME/.local/bin/odo"
 }
