ebca10891eb8dcf5a59aa6adf028dcfd51fd2afc
ecwpz91 <ecwpz91@users.noreply.github.com>
2021-11-08 12:05:44 -0500

    Updated version

--- .local/bin/install-tekton.sh
+++ .local/bin/install-tekton.sh
@@ -1,4 +1,4 @@
 install-tekton() {
-  curl -L 'https://github.com/tektoncd/cli/releases/download/v0.15.0/tkn_0.15.0_Linux_x86_64.tar.gz' \
+  curl -L 'https://github.com/tektoncd/cli/releases/download/v0.21.0/tkn_0.21.0_Linux_x86_64.tar.gz' \
   | tar -xvzf - -C "$HOME/.local/bin" tkn &>/dev/null
 }
