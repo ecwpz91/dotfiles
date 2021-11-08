70f1ea9958b41acc19b7e82885903f2249457fd1
ecwpz91 <ecwpz91@users.noreply.github.com>
2021-11-08 12:05:56 -0500

    Updated version

--- .local/bin/install-virtctl.sh
+++ .local/bin/install-virtctl.sh
@@ -1,5 +1,5 @@
 install-virtctl() {
- curl -L 'https://github.com/kubevirt/kubevirt/releases/download/v0.8.0/virtctl-v0.8.0-linux-amd64' \
+ curl -L 'https://github.com/kubevirt/kubevirt/releases/download/v0.41.0/virtctl-v0.41.0-linux-amd64' \
       -o "$HOME/.local/bin/virtctl" \
  && chmod +x "$HOME/.local/bin/virtctl"
 }
