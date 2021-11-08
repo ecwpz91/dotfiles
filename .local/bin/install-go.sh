ea20bdadf88d9db398a3b78153abf9433922fb4c
ecwpz91 <ecwpz91@users.noreply.github.com>
2021-11-08 12:08:45 -0500

    Upgraded release

--- .local/bin/install-go.sh
+++ .local/bin/install-go.sh
@@ -1,5 +1,5 @@
 install-go() {
- curl -L 'https://dl.google.com/go/go1.11.linux-amd64.tar.gz' \
+ curl -L 'https://golang.org/dl/go1.16.5.linux-amd64.tar.gz' \
  | tar -xvzf - -C "$HOME/.local" --strip 1 &>/dev/null
 
  # https://golang.org/doc/install/source#tools
