0f0eae00fa30a27b91bf9a84f222ba0b02d50151
ecwpz91 <ecwpz91@users.noreply.github.com>
2021-11-08 12:05:04 -0500

    Updated version

--- .local/bin/install-graalvm.sh
+++ .local/bin/install-graalvm.sh
@@ -1,6 +1,6 @@
 install-graalvm() {
  [[ ! -d "$HOME/.local/share/graalvm" ]] && mkdir -p "$HOME/.local/share/graalvm"
 
- curl -L 'https://github.com/oracle/graal/archive/vm-20.2.0.tar.gz' \
+ curl -L 'https://github.com/graalvm/graalvm-ce-builds/releases/download/vm-20.2.0/graalvm-ce-java8-linux-amd64-20.2.0.tar.gz' \
  | tar -xzf - -C "$HOME/.local/share/graalvm" --strip 1
 }
