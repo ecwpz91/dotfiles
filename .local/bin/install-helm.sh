933dba3eeb67458dafd01cc3b82c5bddff983af7
ecwpz91 <ecwpz91@users.noreply.github.com>
2021-11-08 12:05:16 -0500

    Updated version

--- .local/bin/install-helm.sh
+++ .local/bin/install-helm.sh
@@ -1,4 +1,4 @@
 install-helm() {
- curl -L 'https://get.helm.sh/helm-v3.5.0-linux-amd64.tar.gz' \
+ curl -L 'https://get.helm.sh/helm-v3.6.2-linux-amd64.tar.gz' \
  | tar -xvzf - -C "$HOME/.local/bin" --strip 1 &>/dev/null
 }
