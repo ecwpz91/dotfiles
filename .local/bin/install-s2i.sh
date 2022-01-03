669cc8c9b321164762f5459a2e24253642f8819b
ecwpz91 <ecwpz91@users.noreply.github.com>
2022-01-03 10:50:41 -0500

    Updated s2i release

--- .local/bin/install-s2i.sh
+++ .local/bin/install-s2i.sh
@@ -1,5 +1,5 @@
 install-s2i() {
- curl -L 'https://github.com/openshift/source-to-image/releases/download/v1.1.9a/source-to-image-v1.1.9a-40ad911d-linux-amd64.tar.gz' \
+ curl -L 'https://github.com/openshift/source-to-image/releases/download/v1.3.1/source-to-image-v1.3.1-a5a77147-linux-amd64.tar.gz' \
  | tar -xvzf - -C $HOME/.local --strip 1 &>/dev/null
 
  # Install bash completion
