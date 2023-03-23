25c09cc9b335be0871e106190982c810647bd3bd
ecwpz91 <ecwpz91@users.noreply.github.com>
2023-03-23 18:12:37 -0400

    Added flags

--- .local/bin/install-ansible.sh
+++ .local/bin/install-ansible.sh
@@ -4,5 +4,5 @@ install-ansible() {
  pypkgs='pip cryptography paramiko PyYAML simplejson passlib passlib[bcrypt]
          passlib[argon2] passlib[totp] fastpbkdf2 scrypt ansible'
 
- pip install --upgrade --user $pypkgs
+ python -m pip install --upgrade --user $pypkgs
 }
